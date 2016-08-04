ffd2563aff6a4a2797989bcb51daee229c9fd831
Manish Kr. Shukla <manish.shukla@opinioapp.com>
2016-08-04 19:34:49 +0530

    First cut

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /* jshint node: true */
 'use strict';
+var BasePlugin = require('ember-cli-deploy-plugin');
 
 module.exports = {
-  name: 'ember-cli-deploy-dashboard-update'
+  name: 'ember-cli-deploy-dashboard-update',
+
+  createDeployPlugin: function (options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name
+    });
+
+    return new DeployPlugin();
+  },
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
